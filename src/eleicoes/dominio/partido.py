48f6905afa873b064c900cfb5d0a8e3b35deb830
Pedro Mouro <pedromouro78@gmail.com>
2026-05-24 16:43:23 +0100

    Adiciona classe Partido com validações

---
+++ src/eleicoes/dominio/partido.py
@@ -0,0 +1,17 @@
+class Partido:
+    """Representa um partido político concorrente nas eleições."""
+
+    def __init__(self, sigla, nome):
+        if not sigla or not sigla.strip():
+            raise ValueError("Tem de conter a sigla!")
+        if not nome or not nome.strip():
+            raise ValueError("Tem de conter nome!")        
+        self._sigla = sigla
+        self._nome = nome
+    
+    def obter_sigla(self):
+        return self._sigla
+    
+    def obter_nome(self):
+        return self._nome
+    
